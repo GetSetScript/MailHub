e0de65fba67f92bd9943029c0982b14612952ce0
Terra Vermillion <VermillionT@outlook.com>
2019-02-27 09:41:59 -0700

    Cleaned up project, added libriaries, created unit testing project, added libraries to DI and HTTP pipeline, documented

---
+++ MailHub/MailHub/Core/Extensions/IApplicationBuilderExtensions.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Builder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MailHub.Core.Extensions
+{
+    /// <summary>
+    /// Represents a collection of extension methods for the <see cref="IApplicationBuilder"/> class
+    /// </summary>
+    public static class IApplicationBuilderExtensions
+    {
+        /// <summary>
+        /// Configures and adds the middleware for using the generated online API documentation
+        /// </summary>
+        /// <param name="app">The Application Builder to add the middleware to</param>
+        /// <param name="documentTitle">The Title for the online documentation</param>
+        /// <param name="documentVersionName">The Version Name for the online documentation</param>
+        /// <returns>The updated <see cref="IApplicationBuilder"/> instance</returns>
+        public static IApplicationBuilder UseGeneratedDocumentation(this IApplicationBuilder app, string documentTitle, string documentVersionName)
+        {
+            app.UseSwagger(cfg =>
+            {
+                cfg.RouteTemplate = "api/docs/{documentName}/swagger.json";
+            });
+
+            app.UseSwaggerUI(cfg =>
+            {
+                cfg.SwaggerEndpoint($"/api/docs/{documentVersionName}/swagger.json", documentTitle);
+                cfg.RoutePrefix = "api/docs";
+
+                cfg.DocumentTitle = documentTitle;
+                cfg.SupportedSubmitMethods();
+            });
+
+            return app;
+        }
+
+    }
+}
--- MailHub/MailHub/Program.cs
+++ MailHub/MailHub/Program.cs
@@ -7,18 +7,53 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using NLog.Web;
 
 namespace MailHub
 {
+    /// <summary>
+    /// Represents the entry point of the program
+    /// </summary>
     public class Program
     {
+        /// <summary>
+        /// The entry method for the application
+        /// </summary>
+        /// <param name="args">A collection of command line arguments used for optional configuration of the application</param>
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            var logger = NLogBuilder.ConfigureNLog("nlog.config").GetCurrentClassLogger();
+
+            try
+            {
+                logger.Debug("App Started");
+
+                CreateWebHostBuilder(args).Build().Run();
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex, "Program initialization failed due to an exception");
+                throw;
+            }
+            finally
+            {
+                NLog.LogManager.Shutdown();
+            }
         }
 
+        /// <summary>
+        /// Builds the Web Host that is used to run the site
+        /// </summary>
+        /// <param name="args">A collection of arguments used to optionaly configure the Web Host</param>
+        /// <returns>Returns a <see cref="IWebHostBuilder"/> configured to run the website</returns>
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>()
+                .ConfigureLogging(logging =>
+                {
+                    logging.ClearProviders();
+                    logging.SetMinimumLevel(LogLevel.Trace);
+                })
+                .UseNLog();
     }
 }
